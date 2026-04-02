a2396f2fe51e80779a6f8ad84122ba16f41c1a10
ramashishmaurya <ramashishmaurya738@gmail.com>
2026-04-02 12:47:04 +0530

    add more code of Eventlistener

--- javascripts/first.js
+++ javascripts/first.js
@@ -15,19 +15,16 @@
 // here is the div available correct inside the class generate okay  
 const button = document.querySelector(".generate button"); 
 
+
 button.addEventListener("click", function () {
     let h3 = document.querySelector('h3') ; 
     let randomcolors = randomcolor() ; 
-    h3.style.color = randomcolors;
+    h3.style.color.backgroundColor = randomcolors;
     let div = document.querySelector(".abc");
-    div.style.backgroundColor = randomcolors; // use the correct variable name
+    div.style.backgroundColor = 'rgb(53 , 238 ,3)'; // use the correct variable name
+    let body = document.querySelector('body') ;     body.style.backgroundColor = randomcolors; 
     console.log('Color is updated');
 });
-
-
-
-
-
 
 function randomcolor(){
     let red = Math.floor(Math.random()*255) ;
@@ -37,4 +34,17 @@ function randomcolor(){
 
     return color ; 
 }
+let para = document.querySelector('p') ; 
+para.addEventListener('click' ,()=>{
+    console.log('para is clicked ') ;
+}); 
+// this is not only for the paragraphs right this is for the both correct yes 
+
+let box = document.querySelector('.box'); 
+box.addEventListener('mouseenter',()=>{
+    console.log('finally we enter on the mouse ')
+
+}) ; 
+// these is the event listener only the querySelector only select one but here getdocumentbyid and and class select the multiple right we need to select using the array correct 
+
 
