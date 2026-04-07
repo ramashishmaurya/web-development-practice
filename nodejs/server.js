505d3bd85233ed9ce107cfb87076d1868d371431
ramashishmaurya <ramashishmaurya738@gmail.com>
2026-04-07 07:15:46 +0530

    added the feature here

--- nodejs/server.js
+++ nodejs/server.js
@@ -3,7 +3,7 @@ const app = express() ;
 const db = require('./db') ; 
 
 const bodyParser = require('body-parser') ;
-app.use(bodyParser.json()) ; 
+app.use(bodyParser.json()) ;
 
 const person = require('./models/person') ;
 
@@ -25,7 +25,7 @@ app.post('/student' ,(req ,res)=>{
 });
 
 app.post('/person' ,async(req ,res)=>{
-    try{
+    try {
     const data = req.body ; 
     const newPerson = new person(data) ;
 
@@ -40,7 +40,7 @@ app.post('/person' ,async(req ,res)=>{
 
 }) ;
 
-
+// this is for the geeting the data 
 app.get('/person' ,async(req ,res)=>{
     try{
          const data =  await person.find() ;
@@ -53,9 +53,57 @@ app.get('/person' ,async(req ,res)=>{
     }
 })
 
+// 
+app.get('/person/:worktype' ,async(req ,res)=>{
+    try{
+    // the values is th params here 
+    const worktype = req.params.worktype ; 
+    if (worktype =='chef' || worktype=="waiter" || worktype=='manager') {
+        const response = await person.find({work:worktype}) ;
+        res.status(200).json(response) ; 
+    } 
+
+    }catch(errro){
+       console.lo("this is error right ") ;
+       res.status(400).json({erro:"there is server issues"}) ; 
+    }
+
+}) ; 
+// there is logic of routes
+// const personroutes = require('../routes/personRoutes'); 
+
+
+// app.use('/person' , personroutes) ; 
+
+// this is update function
+
+app.put('/person/:id', async (req, res) => {
+    try {
+        const updateid = req.params.id;
+        const updatedata = req.body;
+
+        const response = await person.findByIdAndUpdate(
+            updateid,
+            updatedata,
+            {
+                returnDocument: 'after', // ✅ updated syntax
+                runValidators: true
+            }
+        );
+
+        console.log("Data is updated successfully");
+        res.status(200).json(response);
+
+    } catch (error) {
+        console.log("there is error inside it", error);
+        res.status(400).json({ error: "Something went wrong" });
+    }
+});
+
 
 
 app.listen(8080 ,()=>{
    console.log(`i am listening at port ${8080}`)
 }) ;
 
+// how the updates one can perform that has to make the values as well right 
